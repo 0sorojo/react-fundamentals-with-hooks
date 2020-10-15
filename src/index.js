b30c1921f742720d12a5fa3dc9606b23f103e086
0sorojo <redrootproductions@gmail.com>
2020-10-14 22:18:12 -0400

    single component function creation practice

--- src/index.js
+++ src/index.js
@@ -1,17 +1,60 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const BookList = () => {
+  return (
+    <section className='book-list-container'>
+      <h1>Oprah's Book Club</h1>
+      <div className='book-list'>
+        <Book />
+        <Book />
+        <Book />
+        <Book />
+        <Book />
+        <Book />
+      </div>
+    </section>
+  );
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => {
+  return (
+    <>
+      <img
+        src='https://m.media-amazon.com/images/I/811opppMPQL._AC_UY218_.jpg'
+        alt=''
+      />
+    </>
+  );
+};
+
+const Title = () => {
+  return (
+    <>
+      <h2 className='title'>Caste</h2>
+      <h3>the Origins of Our Discontents</h3>
+    </>
+  );
+};
+
+const Author = () => {
+  return (
+    <>
+      <p>Isabel Wilkerson</p>
+    </>
+  );
+};
+
+render(<BookList />, document.getElementById('root'));
