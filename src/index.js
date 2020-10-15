cba89cc61e8efbc507dae29358a86565af0077d2
0sorojo <redrootproductions@gmail.com>
2020-10-15 13:22:44 -0400

    synthetic event practice

--- src/index.js
+++ src/index.js
@@ -34,14 +34,34 @@ const BookList = () => {
 };
 
 const Book = (props) => {
-  console.log(props);
+  const handleClick = (event) => {
+    console.log(event.target);
+    alert('you clicked me');
+  };
+
+  const complexExample = (event) => {
+    console.log(event);
+    console.log(event.target);
+  };
+
+  const handleMouseOver = () => {
+    console.log(title);
+  };
+
   const { image, title, subTitle, author } = props;
   return (
-    <article className='book'>
-      <img className='image' src={image} alt='' />
-      <h2 className='title'>{title}</h2>
+    <article className='book' onMouseOver={handleMouseOver}>
+      <button type='button' className='x-button' onClick={complexExample}>
+        X
+      </button>
+      <img className='image' src={image} alt='' onClick={handleClick} />
+      <h2 className='title' onClick={handleClick}>
+        {title}
+      </h2>
       <h3>{subTitle}</h3>
-      <p className='author'>{author}</p>
+      <p className='author' onClick={handleClick}>
+        {author}
+      </p>
     </article>
   );
 };
